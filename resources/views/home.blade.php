a9db4e8ce6164010fa6ae3f60bb3bb9e947cf5d2
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-08-29 15:45:44 +0200

    add price

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,8 @@
             <div>
               <div>Data di inizio soggiorno: {{ $travel->stay_start_date }}</div>
               <div>Data di fine soggiorno: {{ $travel->end_of_stay_date }}</div>
-              <div>Info genrerali sul viaggio: {{ $travel->order_info }}</div>     
+              <div>Info genrerali sul viaggio: {{ $travel->order_info }}</div>
+              <div>Prezzo: {{ $travel->total_price }}&euro;</div>  
             </div>
             <br>
         @endforeach
